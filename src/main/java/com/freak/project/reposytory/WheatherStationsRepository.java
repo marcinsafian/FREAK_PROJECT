4aedcb84f508f3cf0daa23023781b727ca322e31
MS <ms@.gmail.com>
2020-02-20 22:46:59 +0100

    problem resolved - delete `@Autowired` annotation in MailService class - should not be there.

--- src/main/java/com/freak/project/reposytory/WheatherStationsRepository.java
+++ src/main/java/com/freak/project/reposytory/WheatherStationsRepository.java
@@ -1,14 +1,9 @@
 package com.freak.project.reposytory;
 
-
-import com.freak.project.domain.Stations;
 import com.freak.project.domain.WheatherStations;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 @Transactional
--- src/main/java/com/freak/project/service/mail/Mail.java
+++ src/main/java/com/freak/project/service/mail/Mail.java
@@ -1,37 +1,45 @@
-//package com.freak.project.service.mail;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@AllArgsConstructor
-//@Getter
-//public class Mail {
-//    private final String sendTo;
-//    private final String subject;
-//    private final String message;
-//
-//    public static class MailBuild {
-//        private  String sendTo;
-//        private  String subject;
-//        private List<String> messageLines = new ArrayList<>();
-//
-//        public MailBuild sendTo(String sendTo){
-//            this.sendTo = sendTo;
-//            return this;
-//        }
-//        public MailBuild subject(String subiect){
-//            this.subject = subiect;
-//            return this;
-//        }
-//        public MailBuild messageLines(String line){
-//            messageLines.add(line);
-//            return this;
-//        }
-//        public Mail build(){
-//            String message = String.join("\n",messageLines);
-//            return new Mail(sendTo,subject,message);
-//        }
-//    }
-//}
+package com.freak.project.service.mail;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public final class Mail {
+    private final String sendTo;
+    private final String subject;
+    private final String message;
+
+    public static class MailBuild {
+        private String sendTo;
+        private String subject;
+        private List<String> messageLine = new ArrayList<>();
+
+        public MailBuild sendTo(String sendTo) {
+            this.sendTo = sendTo;
+            return this;
+        }
+
+        public MailBuild subject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public MailBuild messageLine(String line) {
+            messageLine.add(line);
+            return this;
+        }
+
+        public Mail build() {
+            String message = String.join("\n", messageLine);
+            return new Mail(sendTo, subject, message);
+        }
+    }
+
+    private Mail(final String sendTo, final String subject, String message) {
+        this.sendTo = sendTo;
+        this.subject = subject;
+        this.message = message;
+    }
+}
