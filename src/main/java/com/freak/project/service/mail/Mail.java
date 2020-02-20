6e36c29452c6587326bea045b5d4ad925d946028
MS <ms@.gmail.com>
2020-02-20 21:45:46 +0100

    all test passed

--- src/main/java/com/freak/project/service/mail/Mail.java
+++ src/main/java/com/freak/project/service/mail/Mail.java
@@ -1,37 +1,37 @@
-package com.freak.project.service.mail;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import java.util.ArrayList;
-import java.util.List;
-
-@AllArgsConstructor
-@Getter
-public class Mail {
-    private final String sendTo;
-    private final String subject;
-    private final String message;
-
-    public static class MailBuild {
-        private  String sendTo;
-        private  String subject;
-        private List<String> messageLines = new ArrayList<>();
-
-        public MailBuild sendTo(String sendTo){
-            this.sendTo = sendTo;
-            return this;
-        }
-        public MailBuild subject(String subiect){
-            this.subject = subiect;
-            return this;
-        }
-        public MailBuild messageLines(String line){
-            messageLines.add(line);
-            return this;
-        }
-        public Mail build(){
-            String message = String.join("\n",messageLines);
-            return new Mail(sendTo,subject,message);
-        }
-    }
-}
+//package com.freak.project.service.mail;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@AllArgsConstructor
+//@Getter
+//public class Mail {
+//    private final String sendTo;
+//    private final String subject;
+//    private final String message;
+//
+//    public static class MailBuild {
+//        private  String sendTo;
+//        private  String subject;
+//        private List<String> messageLines = new ArrayList<>();
+//
+//        public MailBuild sendTo(String sendTo){
+//            this.sendTo = sendTo;
+//            return this;
+//        }
+//        public MailBuild subject(String subiect){
+//            this.subject = subiect;
+//            return this;
+//        }
+//        public MailBuild messageLines(String line){
+//            messageLines.add(line);
+//            return this;
+//        }
+//        public Mail build(){
+//            String message = String.join("\n",messageLines);
+//            return new Mail(sendTo,subject,message);
+//        }
+//    }
+//}
--- src/main/java/com/freak/project/service/mail/MailScheduler.java
+++ src/main/java/com/freak/project/service/mail/MailScheduler.java
@@ -1,66 +1,66 @@
-package com.freak.project.service.mail;
-
-import com.freak.project.domain.Stations;
-import com.freak.project.exception.UserNotFoundException;
-import com.freak.project.reposytory.MailSentRepository;
-import com.freak.project.reposytory.UserRepository;
-import com.freak.project.service.ApiWheaderService;
-import com.freak.project.statistic.AllUsersCount;
-import com.freak.project.user.User;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-
-@Component
-public class MailScheduler {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MailScheduler.class);
-    private static final String SUBJECT = "The weather";
-
-    private final MailService mailService;
-    private final UserRepository userRepository;
-    private final ApiWheaderService apiWheaderService;
-    private final MailSentRepository mailSentRepository;
-
-    public MailScheduler(MailService mailService, UserRepository userRepository, ApiWheaderService apiWheaderService, MailSentRepository mailSentRepository) {
-        this.mailService = mailService;
-        this.userRepository = userRepository;
-        this.apiWheaderService = apiWheaderService;
-        this.mailSentRepository = mailSentRepository;
-    }
-    @Scheduled(cron = "0 00 08 * * *")
-    private void sendMail(){
-        LOGGER.info("scheduler start up ");
-        int userCount = 0;
-        int emailCount = 0;
-        for (User user : userRepository.findAll()){
-            userCount++;
-            if(user.isSchedulerOn()){
-                try{
-                    mailService.send(prepareMesageToMail(user));
-                    emailCount++;
-                }catch (Exception e){
-                    LOGGER.error("Sorry We have problem with our post office");
-                }
-            }
-        }
-        mailSentRepository.save(new AllUsersCount(LocalDateTime.now(),userCount,emailCount));
-    }
-    public Mail prepareMesageToMail(User user) throws UserNotFoundException {
-        Stations stations = (Stations) apiWheaderService.fetchByName("");
-        return new Mail.MailBuild().sendTo(user.getEmail()).subject(SUBJECT)
-        .messageLines("\tGood morning, " + user.getFirstName() + "!\n")
-                .messageLines("Are you wondering what the weather is ?")
-                .messageLines(stations.getStacja() + "\n")
-                .messageLines("The weather was updated " + stations.getGodzina_pomiaru())
-                .messageLines("Wind:\t\t\t\t\t   " + stations.getPredkosc_wiatru()+ "m/s " + stations.getKierunek_wiatru())
-                .messageLines("Air temperature:\t\t\t" + stations.getTemperatura() + "\u00B0C")
-                .messageLines("Air pressure:\t\t  " + stations.getCisnienie()+ "MPa")
-                .messageLines("\nHave a nice day!")
-                .build();
-    }
-}
+//package com.freak.project.service.mail;
+//
+//import com.freak.project.domain.Stations;
+//import com.freak.project.exception.UserNotFoundException;
+//import com.freak.project.reposytory.MailSentRepository;
+//import com.freak.project.reposytory.UserRepository;
+//import com.freak.project.service.ApiWheaderService;
+//import com.freak.project.statistic.AllUsersCount;
+//import com.freak.project.user.User;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//
+//@Component
+//public class MailScheduler {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(MailScheduler.class);
+//    private static final String SUBJECT = "The weather";
+//
+//    private final MailService mailService;
+//    private final UserRepository userRepository;
+//    private final ApiWheaderService apiWheaderService;
+//    private final MailSentRepository mailSentRepository;
+//
+//    public MailScheduler(MailService mailService, UserRepository userRepository, ApiWheaderService apiWheaderService, MailSentRepository mailSentRepository) {
+//        this.mailService = mailService;
+//        this.userRepository = userRepository;
+//        this.apiWheaderService = apiWheaderService;
+//        this.mailSentRepository = mailSentRepository;
+//    }
+//    @Scheduled(cron = "0 00 08 * * *")
+//    private void sendMail(){
+//        LOGGER.info("scheduler start up ");
+//        int userCount = 0;
+//        int emailCount = 0;
+//        for (User user : userRepository.findAll()){
+//            userCount++;
+//            if(user.isSchedulerOn()){
+//                try{
+//                    mailService.send(prepareMesageToMail(user));
+//                    emailCount++;
+//                }catch (Exception e){
+//                    LOGGER.error("Sorry We have problem with our post office");
+//                }
+//            }
+//        }
+//        mailSentRepository.save(new AllUsersCount(LocalDateTime.now(),userCount,emailCount));
+//    }
+//    public Mail prepareMesageToMail(User user) throws UserNotFoundException {
+//        Stations stations = (Stations) apiWheaderService.fetchByName("");
+//        return new Mail.MailBuild().sendTo(user.getEmail()).subject(SUBJECT)
+//        .messageLines("\tGood morning, " + user.getFirstName() + "!\n")
+//                .messageLines("Are you wondering what the weather is ?")
+//                .messageLines(stations.getStacja() + "\n")
+//                .messageLines("The weather was updated " + stations.getGodzina_pomiaru())
+//                .messageLines("Wind:\t\t\t\t\t   " + stations.getPredkosc_wiatru()+ "m/s " + stations.getKierunek_wiatru())
+//                .messageLines("Air temperature:\t\t\t" + stations.getTemperatura() + "\u00B0C")
+//                .messageLines("Air pressure:\t\t  " + stations.getCisnienie()+ "MPa")
+//                .messageLines("\nHave a nice day!")
+//                .build();
+//    }
+//}
